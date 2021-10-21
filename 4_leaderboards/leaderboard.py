9f4e9d3a954661270c57e439923e678d4b9d9409
Helene Brulin <helene.brulin@redislabs.com>
2021-10-21 16:29:32 +0200

    add scores and cut string

--- 4_leaderboards/leaderboard.py
+++ 4_leaderboards/leaderboard.py
@@ -5,15 +5,15 @@ import sys
 import redis
 import threading
 
-client = redis.Redis(host="redis-12000.internal.helene-eu-cluster.demo.redislabs.com", port=12000)
+client = redis.StrictRedis(host="redis-12000.internal.helene-eu-cluster.demo.redislabs.com", port=12000, charset="utf-8", decode_responses=True)
 
 
 def get_top():
     threading.Timer(1.0, get_top).start()
     print('\n')
-    top = client.zrevrange("ldboard", 0, 4)
+    top = client.zrevrange("ldboard", 0, 4, withscores=True)
     for idx, player in enumerate(top):
-        print(idx + 1, ' - ', player[1:])
+        print(idx + 1, ' - ', player)
 
 
 get_top()
